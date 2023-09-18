7fd1444b7b9b03b0caeb20adce5e004fd0693a89
BZAgustin <abustoszanelli@gmail.com>
2023-09-18 18:34:43 -0300

    Add extra validation and turn 'artist' and 'genre' values into arrays

--- models/album.js
+++ models/album.js
@@ -3,13 +3,15 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const AlbumSchema = new Schema({
-  name: { type: String, required: true },
-  artist: { type: Schema.Types.ObjectId, ref: 'Artist', required: true },
+  name: { type: String, 
+          required: true, 
+          minlength: 1,
+          maxlength: 100
+        },
+  artist: [{ type: Schema.Types.ObjectId, ref: 'Artist', required: true }],
   released: { type: Date },
   cover: { type: String },
-  genre: [
-    { type: Schema.Types.ObjectId, ref: 'Genre'}
-  ],
+  genre: [{ type: Schema.Types.ObjectId, ref: 'Genre' }],
   ISBN: { type: String },
   label: { type: Schema.Types.ObjectId, ref: 'Label' },
   tracklist: { type: String } 
