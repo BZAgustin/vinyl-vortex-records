e5422765dd39b6141ab5da66b62e5aa21a078493
BZAgustin <abustoszanelli@gmail.com>
2023-10-04 10:55:24 -0300

    Add album instance controller

---
+++ controllers/albumInstanceController.js
@@ -0,0 +1,33 @@
+const album = require('../models/album');
+
+exports.albumInstanceList = asyncHandler(async (req, res, next) => {
+  res.send('NOT IMPLEMENTED: Album Instance List');
+});
+
+exports.albumInstanceDetail = asyncHandler(async (req, res, next) => {
+  res.send('NOT IMPLEMENTED: Album Instance Detail');
+});
+
+exports.albumInstanceCreateGet = asyncHandler(async (req, res, next) => {
+  res.send('NOT IMPLEMENTED: Album Instance Create GET');
+});
+
+exports.albumInstanceCreatePost = asyncHandler(async (req, res, next) => {
+  res.send('NOT IMPLEMENTED: Album Instance Create POST');
+});
+
+exports.albumInstanceDeleteGet = asyncHandler(async (req, res, next) => {
+  res.send('NOT IMPLEMENTED: Album Instance Delete GET');
+});
+
+exports.albumInstanceDeletePost = asyncHandler(async (req, res, next) => {
+  res.send('NOT IMPLEMENTED: Album Instance Delete POST');
+});
+
+exports.albumInstanceUpdateGet = asyncHandler(async (req, res, next) => {
+  res.send('NOT IMPLEMENTED: Album Instance Update GET');
+});
+
+exports.albumInstanceUpdatePost = asyncHandler(async (req, res, next) => {
+  res.send('NOT IMPLEMENTED: Album Instance Update POST');
+});
