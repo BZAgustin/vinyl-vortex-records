352723aac55f141aa8aed37424804ee2c4990785
BZAgustin <abustoszanelli@gmail.com>
2023-10-22 13:04:31 -0300

    Add handler for creating album instances

--- controllers/albumInstanceController.js
+++ controllers/albumInstanceController.js
@@ -1,6 +1,8 @@
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 const AlbumInstance = require('../models/albumInstance');
+const Album = require('../models/album');
 
 exports.albumInstanceList = asyncHandler(async (req, res, next) => {
   const albumInstances = await AlbumInstance.find({}).exec();
@@ -13,7 +15,7 @@ exports.albumInstanceList = asyncHandler(async (req, res, next) => {
     },
   });
 
-  res.render("store", { title: "Store", albumInstanceList: albumsInStock })
+  res.render('store', { title: 'Store', albumInstanceList: albumsInStock })
 });
 
 exports.albumInstanceDetail = asyncHandler(async (req, res, next) => {
@@ -31,12 +33,57 @@ exports.albumInstanceDetail = asyncHandler(async (req, res, next) => {
 });
 
 exports.albumInstanceCreateGet = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Album Instance Create GET');
+  const allAlbums = await Album.find({}, "title").exec();
+
+  res.render('albumInstanceCreate', {
+    title: 'Add Album Copy',
+    albums: allAlbums,
+  });
 });
 
-exports.albumInstanceCreatePost = asyncHandler(async (req, res, next) => {
-  res.send('NOT IMPLEMENTED: Album Instance Create POST');
-});
+exports.albumInstanceCreatePost = [
+  body('album')
+    .trim()
+    .isLength({ min: 1, max: 100 })
+    .escape()
+    .withMessage('Title is required'),
+  body('condition')
+    .trim()
+    .isLength({ min: 2, max: 60 })
+    .escape()
+    .withMessage('Artist is required'),
+  body('price', 'Invalid price')
+    .isNumeric()
+    .escape()
+    .withMessage('Price must be a number.'),
+
+    asyncHandler(async (req, res, next) => {
+      const errors = validationResult(req);
+    
+      const albumInstance = new AlbumInstance({
+        album: req.body.album,
+        condition: req.body.condition,
+        price: req.body.price
+      });
+
+      if (!errors.isEmpty()) {
+        const allAlbums = await Album.find({}, 'title').exec();
+
+        res.render('albumInstanceForm', {
+          title: 'Add Album Copy',
+          albumList: allAlbums,
+          selectedAlbum: albumInstance.album._id,
+          errors: errors.array(),
+          albumInstance,
+        });
+        return;
+      } else {
+        await albumInstance.save();
+        res.redirect(albumInstance.url);
+      }
+    })
+];
+
 
 exports.albumInstanceDeleteGet = asyncHandler(async (req, res, next) => {
   res.send('NOT IMPLEMENTED: Album Instance Delete GET');
