17724d3203281ccc70b4c48d3559ea8045d810a8
BZAgustin <abustoszanelli@gmail.com>
2023-10-23 12:02:24 -0300

    Fix spacing

--- routes/catalog.js
+++ routes/catalog.js
@@ -11,15 +11,12 @@ const genreController = require('../controllers/genreController');
 router.get('/', albumController.index);
 
 router.get('/album/create', albumController.albumCreateGet);
-
 router.post('/album/create', albumController.albumCreatePost);
 
 router.get('/album/:id/delete', albumController.albumDeleteGet);
-
 router.post('/album/:id/delete', albumController.albumDeletePost);
 
 router.get('/album/:id/update', albumController.albumUpdateGet);
-
 router.post('/album/:id/update', albumController.albumUpdatePost);
 
 router.get('/album/:id', albumController.albumDetail);
@@ -29,15 +26,12 @@ router.get('/albums', albumController.albumList);
 // -------------------- ARTIST ROUTES -------------------- //
 
 router.get('/artist/create', artistController.artistCreateGet);
-
 router.post('/artist/create', artistController.artistCreatePost);
 
 router.get('/artist/:id/delete', artistController.artistDeleteGet);
-
 router.post('/artist/:id/delete', artistController.artistDeletePost);
 
 router.get('/artist/:id/update', artistController.artistUpdateGet);
-
 router.post('/artist/:id/update', artistController.artistUpdatePost);
 
 router.get('/artist/:id', artistController.artistDetail);
@@ -47,15 +41,12 @@ router.get('/artists', artistController.artistList);
 // -------------------- GENRE ROUTES -------------------- //
 
 router.get('/genre/create', genreController.genreCreateGet);
-
 router.post('/genre/create', genreController.genreCreatePost);
 
 router.get('/genre/:id/delete', genreController.genreDeleteGet);
-
 router.post('/genre/:id/delete', genreController.genreDeletePost);
 
 router.get('/genre/:id/update', genreController.genreUpdateGet);
-
 router.post('/genre/:id/update', genreController.genreUpdatePost);
 
 router.get('/genre/:id', genreController.genreDetail);
@@ -64,22 +55,19 @@ router.get('/genres', genreController.genreList);
 
 // -------------------- ALBUM INSTANCE ROUTES -------------------- //
 
-router.get('/albuminstance/create', albumInstanceController.albumInstanceCreateGet);
-
-router.post('/albuminstance/create', albumInstanceController.albumInstanceCreatePost);
+router.get('/albuminstance/create/', albumInstanceController.albumInstanceCreateGet);
+router.post('/albuminstance/create/', albumInstanceController.albumInstanceCreatePost);
 
 router.get('/albuminstance/:id/delete', albumInstanceController.albumInstanceDeleteGet);
-
 router.post('/albuminstance/:id/delete', albumInstanceController.albumInstanceDeletePost);
 
 router.get('/albuminstance/:id/update', albumInstanceController.albumInstanceUpdateGet);
-
 router.post('/albuminstance/:id/update', albumInstanceController.albumInstanceUpdatePost);
 
 router.get('/albuminstance/:id', albumInstanceController.albumInstanceDetail);
 
 router.get('/albuminstances', albumInstanceController.albumInstanceList);
 
-router.get('/store', albumInstanceController.albumInstanceList)
+router.get('/store', albumInstanceController.albumInstanceList);
 
 module.exports = router;
